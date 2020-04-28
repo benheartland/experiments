d9504464aec04527c144527f9d12c0689970530d
Ben Heartland <ben@symplectic.co.uk>
2020-04-29 00:46:19 +0100

    Octaver: WIP on audio device list and track selector.

--- octaver/script.js
+++ octaver/script.js
@@ -6,21 +6,22 @@ function plural(n, singular, plural = singular + 's') {
   return n == 1 ? n + ' ' + singular : n + ' ' + plural;
 }
 
-const supportedConstraint = navigator.mediaDevices.getSupportedConstraints();
-
 // Set up an array of objects that will be looped over on media device change.
 // If an object in the list has a onmediadevicechange() method, it will be called.
-let onMediaDeviceChangeList = new Array();
+window.onMediaDeviceChangeList = new Array();
 // Event listener that will loop over objects registered in onMediaDeviceChangeList() and call their onmediadevicechange() method, if it exists
 navigator.mediaDevices.ondevicechange = function() {
   // DEBUG
   console.log('mediaDevices.ondevicechange called')
-  // 
-  onMediaDeviceChangeList
+  // loop through the objects in onMediaDeviceChangeList
+  window.onMediaDeviceChangeList
+    // For each object, check that onmediadevicechange exists and is a function, then execute it
     .filter( function(_object) {return typeof(_object.onmediadevicechange) === 'function' } )
     .forEach( function(_object) {_object.onmediadevicechange()} );
 }
 
+// Get the mediaDevices constraints supported by the browser
+const supportedConstraint = navigator.mediaDevices.getSupportedConstraints();
 // Construct the set of constraints we want to ask from the audio streams
 let standardAudioConstraints = {};
 if(supportedConstraint.autoGainControl) standardAudioConstraints.autoGainControl = {ideal: false};
@@ -30,15 +31,97 @@ if(supportedConstraint.sampleRate) standardAudioConstraints.sampleRate = {min: 2
 if(supportedConstraint.sampleSize) standardAudioConstraints.sampleSize = {min: 8, ideal: 24, max: 32};
 if(supportedConstraint.latency) standardAudioConstraints.latency = {min: 0.0, ideal: 0.0, max: 2.0};
 
+// A list of media devices of the specified kind (or all media devices if kind is not specified).
+class MediaDeviceList {
+
+  static allowedKindValues = ['videoinput', 'audioinput', 'audiooutput'];
+  static excludeDeviceIds = ['default', 'communications'];
+
+  constructor(_kind) {
+    // What kind of media devices does this list contain (null => all devices, videoinput, audioinput, audiooutput)
+    this.kind = _kind;
+    this.item = new Array();
+    // An array of dependent objects. Any with
+    this.dependents = new Array();
+  }
+
+  // length property
+  get length() {
+    return this.item.length;
+  }
+
+  set kind(_value) {
+    if(_value == null || MediaDeviceList.allowedKindValues.includes(_value)) {this._kind = _value;}
+    else {throw new Error(`Invalid value "${_value}" passed for MediaDeviceList.kind. Allowed values are "${MediaDeviceList.allowedKindValues.join('", "')}" or null.`);}
+  }
+
+  get kind() {return this._kind;}
+
+  // METHODS
+
+  // Add a forEach() method
+  forEach(_callback) {
+    this.item.forEach(_callback);
+  }
+
+  // Clear the list
+  clear() {
+    this.item.splice(0, this.item.length);
+  }
+
+  // Register a dependent object
+  registerDependent(_object) {
+    this.dependents.push(_object);
+  }
+
+  // Clear all dependents
+  clearDependents() {
+    this.dependents.splice(0, this.dependents.length);
+  }
+
+  // Update the media device list
+  update() {
+    var _thisList = this;
+    // enumerateDevices promise
+    navigator.mediaDevices.enumerateDevices()
+    .then(function(_devices) {
+      // restrict the enumerated devices to the specified kind (if any is specified). Exclude default and communications devices (these will be listed as physical devices anyway).
+      _devices = _devices.filter( _device => !(MediaDeviceList.excludeDeviceIds.includes(_device.deviceId)) && (!_thisList.kind || _device.kind === _thisList.kind) );
+      // Remove existing devices that are not in the new array
+      _thisList.item.filter( _item => !(_devices.map(d => d.deviceId).includes(_item.deviceId)) ).forEach( function(_item, _index) {_thisList.item.splice(_index, 1)} );
+      // Add new devices that are not already in the existing array
+      _devices.filter( _device => !(_thisList.item.map(i => i.deviceId).includes(_device.deviceId)) ).forEach( function(_device) {_thisList.item.push(_device)} );
+      // Call the update() method of any dependent objects that have one
+      _thisList.dependents.filter( _object => typeof(_object.update) === 'function' ).forEach( function(_object) {_object.update()} );
+
+      // DEBUG
+      console.log(_thisList);
+
+    })
+    .catch(function(_err) {
+        console.log('Error enumerating media devices.');
+        throw _err;
+    })
+  }
+
+  // the onmediadevicechange method for this object
+  onmediadevicechange() {
+    this.update();
+  }
+
+}
+
 // requests access to all audio input media devices and creates a collection of their media stream audio tracks
 class AudioInputDevicesMediaStreamTrackCollection {
 
-  constructor(_successCallbackFunction = null, _failureCallbackFunction = null) {
+  constructor(_mediaDeviceList) {
+    this.mediaDeviceList = _mediaDeviceList;
     this.item = new Array();
     // an array of objects that are dependent on this. If the object has an update() method, it will be called
     // when this object's update() method is called. 
     this.dependents = new Array();
-    this.update(_successCallbackFunction, _failureCallbackFunction);
+    // Register this track collection as a dependent of its Device List
+    this.mediaDeviceList.registerDependent(this);
   }
 
   // the length property
@@ -60,84 +143,57 @@ class AudioInputDevicesMediaStreamTrackCollection {
     this.item.forEach(_callback);
   }
 
-  // Register a dependent object
-  registerDependent(_object) {
-    this.dependents.push(_object);
-  }
-
   // Clear the track list
   clear() {
     this.item.splice(0, this.item.length);
   }
 
-  // update the list with the latest audio input devices
-  update(_successCallbackFunction = null, _failureCallbackFunction = null) {
+  // Register a dependent object
+  registerDependent(_object) {
+    this.dependents.push(_object);
+  }
+  // Clear all dependents
+  clearDependents() {
+    this.dependents.splice(0, this.dependents.length);
+  }
+
+  // update the list with the audio input tracks available from _mediaDeviceList
+  update() {
+
     // backreference to this (needed later in promise handlers)
     var _this = this;
-    // local variable
+    // Array of getUserMedia promises
     var _getUserMediaPromiseList = new Array();
-    // TODO: Instead of clearing the device list and starting again, only add/remove devices that have changed
-    // TODO: This will avoid re-requested access permission that has already been given
-    // Clear the device list
+    // TODO: change this so that (a) it works and (b) getUserMedia() is called only when a new tracks is added
+    // Clear the track list (TODO: lose this line)
     this.clear();
-    // List the audio devices
-    navigator.mediaDevices.enumerateDevices()
-    .then(function(_devices) {
-      // Filter to audio inputs
-      _audioInputDevicesList = _devices.filter(function(_device) {return _device.kind === 'audioinput'});
-      // Cycle through audio input devices
-      _audioInputDevicesList.forEach(function(_device) {
-        // Request access to the device's audio input stream
-        _getUserMediaPromiseList.push(
-          navigator.mediaDevices.getUserMedia({audio: {deviceId: _device.deviceId}})
-          .then(function(_mediaStream) {
-            // Cycle through the stream's audio tracks
-            _mediaStream.getAudioTracks().forEach(function(_track) {
-              // add the track to the collection
-              _this.item.push(_track);
-            })
+    // Cycle through audio input devices
+    this.mediaDeviceList.forEach(function(_device) {
+      // Request access to the device's audio input stream
+      _getUserMediaPromiseList.push(
+        navigator.mediaDevices.getUserMedia({audio: {deviceId: _device.deviceId}})
+        .then(function(_mediaStream) {
+          // Cycle through the stream's audio tracks
+          _mediaStream.getAudioTracks().forEach(function(_track) {
+            // add the track to the collection
+            _this.item.push(_track);
           })
-          .catch(function(_err) {
-            console.log('Error getting user media for device with id ' + _device.deviceId);
-            console.error(_err);
-          })
-        );
-      })
+        })
+        .catch(function(_err) {
+          console.log('Error getting user media for device with id ' + _device.deviceId);
+          console.error(_err);
+        })
+      );
     })
-    .catch(function(_err) {
-      console.log('Error enumerating media devices.');
-      console.error(_err);
-      // If _failureCallbackFunction is defined, call it
-      if(_failureCallbackFunction) _failureCallbackFunction();
-    })
-    .finally(function() {
-      // Wait until all the getUserMediaPromises have been settled
-      Promise.allSettled(_getUserMediaPromiseList)
-      .then(function() {
-        // If this has returned at least one track and _successCallbackFunction is defined, call it
-        if(_this.length > 0 && _successCallbackFunction) _successCallbackFunction()
-        // Otherwise, if _failureCallbackFunction is defined, call it
-        else if(_failureCallbackFunction) _failureCallbackFunction(); 
-        // for each registered dependent that has an update() method, call it 
-        _this.dependents
-          .filter( function(d) {return typeof(d.update) === 'function'} )
-          .forEach( function(d) {d.update()} );
-      })
+
+    // Wait until all the getUserMediaPromises have been settled
+    Promise.allSettled(_getUserMediaPromiseList)
+    .then(function() {
+      // Call the update() method of any dependent objects that have one
+      _this.dependents.filter( function(_object) {return typeof(_object.update) === 'function'} ).forEach( function(_object) {_object.update()} );
     });
 
   } // end of update() method
-
-  onmediadevicechange() {
-    var _this = this
-    this.update(
-      // update success callback function
-      function() {
-        _this.dependents.forEach(function(d) {
-          d.onmediadevicechange();
-        })
-      }
-    )
-  }
 
 }
 
@@ -149,12 +205,12 @@ class AudioInputDevicesMediaStreamTrackSelector {
   // _inSelectionChangeFunction : a callback function to be called when the selection is changed
   constructor(_audioInputDevicesMediaStreamTrackCollection, _onSelectionChangeFunction) {
     // properties
-    this.availableTracks = _audioInputDevicesMediaStreamTrackCollection;
+    this.audioTrackCollection = _audioInputDevicesMediaStreamTrackCollection;
+    // register this selector as being dependent on the audio track collection
+    this.audioTrackCollection.registerDependent(this);
+    // Start with no track selected
     this.selectedTrack = null;
     this.onSelectionChange = _onSelectionChangeFunction;
-
-    // register this selector as being dependent on the audio track collection
-    _audioInputDevicesMediaStreamTrackCollection.registerDependent(this);
 
     // A container <div>
     this.container = document.body.appendChild(document.createElement('div'));
@@ -175,7 +231,7 @@ class AudioInputDevicesMediaStreamTrackSelector {
     // table body
     this.tbody = this.table.createTBody();
     // Call the update() method to update the selector
-    this.update()
+    this.update();
 
   } // end of constructor
 
@@ -187,7 +243,7 @@ class AudioInputDevicesMediaStreamTrackSelector {
     // clear the selector table body
     this.tbody.innerHTML = '';
     // loop over the available audio tracks
-    this.availableTracks.forEach(
+    this.audioTrackCollection.forEach(
       function(_track) {
         // Get the track's settings
         var _trackSettings = _track.getSettings();
@@ -218,43 +274,53 @@ class AudioInputDevicesMediaStreamTrackSelector {
     )
   }
 
-  // what happens to this selector when media devices change
-  onmediadevicechange() {
-    this.update();
-  }
-
 } // end of AudioInputsSelector class declaration
 
 window.onload = function() {
 
-  // Request access to audio input devices and make a collection of available audio input tracks 
-  window.audioInputDevicesMediaStreamTrackCollection = new AudioInputDevicesMediaStreamTrackCollection(
-    // success callback function
-    function() {
-      // register the track collection in the onMediaDeviceChangeList
-      onMediaDeviceChangeList.push(window.audioInputDevicesMediaStreamTrackCollection);
+  // Create an audio input device list object (not yet populated)
+  window.audioInputMediaDeviceList = new MediaDeviceList('audioinput');
+  // register the device list in the onMediaDeviceChangeList
+  window.onMediaDeviceChangeList.push(window.audioInputMediaDeviceList);
 
-      // create an AudioContext
-      var audioCtx = new AudioContext();
+  // Make a collection of available audio input tracks tied to the device list.
+  window.audioInputDevicesMediaStreamTrackCollection = new AudioInputDevicesMediaStreamTrackCollection(window.audioInputMediaDeviceList);
 
-      // initialise a variable for a MediaStreamTrackSourceNode
-      var mediaStreamTrackSourceNode;
-      // create a track selector
-      var selector = new AudioInputDevicesMediaStreamTrackSelector(
-        // The AudioInputDevicesMediaStreamTrackCollection to use in the selector
-        window.audioInputDevicesMediaStreamTrackCollection
-        // The selector's onSelectionChange function
-        ,function() {
-          // TODO: for now, just log the selection. Eventually do something more meaningful with the selected track
-          console.log('Track selected: ' + this.selectedTrack.label);
-//          mediaStreamTrackSourceNode = audioCtx.createMediaStreamTrackSource(selector.selectedTrack);
-        }
-      );
-    }
-    // failure callback function
-    ,function() {
-      // TODO: say/do something more useful here
-      alert('Something went wrong!')
+  // Make a selector for the audio input track collection.
+  // create a track selector
+  window.audioInputTrackSelector = new AudioInputDevicesMediaStreamTrackSelector(
+    window.audioInputDevicesMediaStreamTrackCollection,  // The AudioInputDevicesMediaStreamTrackCollection to use in the selector
+    function() {  // The selector's onSelectionChange callback function
+      // TODO: for now, just log the selection. Eventually do something more meaningful with the selected track
+      console.log('Track selected: ' + this.selectedTrack.label);
+//      mediaStreamTrackSourceNode = audioCtx.createMediaStreamTrackSource(selector.selectedTrack);
     }
   );
+
+  // Populate the audio input device list
+  window.audioInputMediaDeviceList.update();
+
+/*
+
+  .then(
+
+    // success callback function
+      function() {
+
+        // create an AudioContext
+        var audioCtx = new AudioContext();
+
+        // initialise a variable for a MediaStreamTrackSourceNode
+        var mediaStreamTrackSourceNode;
+      }
+      // failure callback function
+      ,function() {
+        // TODO: say/do something more useful here
+        alert('Something went wrong!')
+      }
+    )
+  )
+
+*/
+
 };
